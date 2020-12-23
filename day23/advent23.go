b1befe6a6b93a5a6a1c7891d42bcfaf3ea2977df
Xavier Sala <utrescu@gmail.com>
2020-12-23 13:35:03 +0100

    day 23 intent amb punters

--- day23/advent23.go
+++ day23/advent23.go
@@ -8,52 +8,37 @@ import (
 	"strconv"
 )
 
-func stringToNumbersRing(input string) *ring.Ring {
+func stringToNumbers(input string) []int {
 
-	circle := ring.New(len(input))
-	for _, value := range input {
+	result := make([]int, len(input))
+	for index, value := range input {
 		number, err := strconv.Atoi(string(value))
 		if err != nil {
 			panic("Bad input")
 		}
-		circle.Value = number
+		result[index] = number
+	}
+	return result
+}
+
+func ringAppend(values []int) *ring.Ring {
+	circle := ring.New(len(values))
+	for _, value := range values {
+		circle.Value = value
 		circle = circle.Next()
 	}
 	return circle
 }
 
-func findDestination(current int, numbers []int) (int, int) {
-	max := 0
-	maxIndex := -1
-	proper := -1
-	properIndex := -1
-	for i, candidate := range numbers {
-		if candidate > max {
-			max = candidate
-			maxIndex = i
-		}
-		if candidate < current {
-			if candidate > proper {
-				proper = candidate
-				properIndex = i
-			}
-		}
-	}
-
-	if proper == -1 {
-		return maxIndex, max
-	}
-	return properIndex, proper
-}
-
-func ringString(anell *ring.Ring) string {
+func ringToString(anell *ring.Ring) string {
 	resultat := ""
 	anell.Do(func(p interface{}) {
 		resultat += strconv.Itoa(p.(int))
 	})
 	return resultat
 }
-func printRing(text string, anell *ring.Ring) {
+
+func ringPrint(text string, anell *ring.Ring) {
 	fmt.Print(text)
 	anell.Do(func(p interface{}) {
 		fmt.Print(p.(int))
@@ -61,7 +46,7 @@ func printRing(text string, anell *ring.Ring) {
 	fmt.Println()
 }
 
-func locateDestination(origen int, anell *ring.Ring) *ring.Ring {
+func ringLocateDestination(origen int, anell *ring.Ring) *ring.Ring {
 	max := anell
 	maxValue := -1
 	minMax := anell
@@ -87,7 +72,7 @@ func locateDestination(origen int, anell *ring.Ring) *ring.Ring {
 	return max
 }
 
-func locateValueInRing(searched int, anell *ring.Ring) (*ring.Ring, error) {
+func ringLocateValue(searched int, anell *ring.Ring) (*ring.Ring, error) {
 	iterator := anell
 	n := iterator.Len()
 	for i := 0; i < n; i++ {
@@ -99,34 +84,157 @@ func locateValueInRing(searched int, anell *ring.Ring) (*ring.Ring, error) {
 	return nil, errors.New("not found")
 }
 
-func barreja(input string, vegades int) string {
-	anell := stringToNumbersRing(input)
+func barreja(numbers []int, vegades int) *ring.Ring {
+	anell := ringAppend(numbers)
 
 	moves := 0
-
 	for moves < vegades {
 		agafats := anell.Unlink(3)
-		destination := locateDestination(anell.Value.(int), anell)
+		destination := ringLocateDestination(anell.Value.(int), anell)
 		destination.Link(agafats)
 		anell = anell.Next()
 		moves++
 	}
+	return anell
+}
 
+func barreja1Ring(input string, vegades int) string {
+	numbers := stringToNumbers(input)
+
+	anell := barreja(numbers, vegades)
 	// Localitzar el número 1
-	result, err := locateValueInRing(1, anell)
+	result, err := ringLocateValue(1, anell)
 	if err != nil {
 		panic(err.Error())
 	}
-	solution := ringString(result)
+	solution := ringToString(result)
 
 	return solution[1:]
+}
+
+func barreja1(input string, size int, vegades int) string {
+
+	inputs := stringToNumbers(input)
+	numbers := make([]number, size)
+	numbers[0] = number{inputs[0], nil}
+	one := 0
+
+	current := &numbers[0]
+	var value int
+	for i := 1; i < size; i++ {
+		numbers[i] = number{inputs[i], nil}
+		if value == 1 {
+			one = i
+		}
+		current.next = &numbers[i]
+		current = current.next
+	}
+	numbers[size-1].next = &numbers[0]
+
+	numbers = barrejaNoRing(numbers, vegades)
+
+	start := numbers[one].next
+	solution := ""
+	for start.value != 1 {
+		solution += strconv.Itoa(start.value)
+		start = start.next
+	}
+
+	return solution
+}
+
+type number struct {
+	value int
+	next  *number
+}
+
+func locateDestination(actual *number, value int) *number {
+	minMax := &number{-1, nil}
+	max := &number{-1, nil}
+	start := actual.next
+	for start.value != value {
+		currentValue := start.value
+		if currentValue == value-1 {
+			return start
+		}
+		if currentValue < value && currentValue > minMax.value {
+			minMax = start
+		} else if currentValue > max.value {
+			max = start
+		}
+		start = start.next
+	}
+	if minMax.value != -1 {
+		return minMax
+	}
+	return max
+}
+
+func barrejaNoRing(input []number, vegades int) []number {
+
+	current := &input[0]
+	moves := 0
+	for moves < vegades {
+		// Remove
+		firstRemoved := current.next
+		lastRemoved := firstRemoved.next.next
+		current.next = lastRemoved.next
+		// append removeds
+		destination := locateDestination(current, current.value)
+		afterdestination := destination.next
+		destination.next = firstRemoved
+		lastRemoved.next = afterdestination
+		// step
+		current = current.next
+
+		moves++
+	}
+	return input
+}
+
+func barreja2(input string, quantitat int, vegades int) int {
+	inputs := stringToNumbers(input)
+
+	numbers := make([]number, quantitat)
+
+	numbers[0] = number{inputs[0], nil}
+	one := 0
+
+	current := &numbers[0]
+	var value int
+	for i := 1; i < quantitat; i++ {
+		if i < len(inputs) {
+			value = inputs[i]
+		} else {
+			value = i
+		}
+		numbers[i] = number{value, nil}
+		if value == 1 {
+			one = i
+		}
+		current.next = &numbers[i]
+		current = current.next
+	}
+	numbers[quantitat-1].next = &numbers[0]
+
+	result := barrejaNoRing(numbers, vegades)
+
+	primer := result[one].next.value
+	segon := result[one].next.next.value
+
+	return primer * segon
 }
 
 func main() {
 
 	// numeros := "389125467"
 	numeros := "157623984"
-	resultat1 := barreja(numeros, 100)
+
+	resultat1a := barreja1(numeros, len(numeros), 100)
+	fmt.Println("Part 1:", resultat1a)
+	resultat1 := barreja1Ring(numeros, 100)
 	fmt.Println("Part 1:", resultat1)
 
+	resultat2 := barreja2(numeros, 1000000, 10000000)
+	fmt.Println("Part 2:", resultat2)
 }
